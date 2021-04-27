1111fa93e5c7c170348ed965e9346e710b6d179d
Lukas Sekoulidis <84957@beuth-hochschule.de>
2021-04-27 13:29:31 +0200

    3.2 normieren hinzugefügt

--- src/cgg/a03/Lochkamera.java
+++ src/cgg/a03/Lochkamera.java
@@ -19,7 +19,17 @@ public class Lochkamera {
         double x = xA - width/2;
         double y = height/2 - yA;
         double z = -(width/2)/Math.tan(phi/2);
+        double normieren = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
+        if(x != 0){
+            x /= normieren;
+        }
+        if(y != 0){
+            y /= normieren;
+        }
+        if(z != 0){
+            z /= normieren;
+        }
         Direction d = Vector.direction(x, y, z);
-        return new Ray(ursprung, d, 1,1);
+        return new Ray(ursprung, d, 0, 0);
     }
 }
