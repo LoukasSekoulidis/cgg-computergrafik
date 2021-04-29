5d0fe562ed8f2c4418ccb073a489a0aeef051221
Lukas Sekoulidis <84957@beuth-hochschule.de>
2021-04-29 09:44:03 +0200

    Hit Attribut verbessert

--- src/cgg/a03/Hit.java
+++ src/cgg/a03/Hit.java
@@ -5,10 +5,10 @@ import cgtools.*;
 public class Hit {
     Double t;
     Point x;
-    Vector n;
+    Direction n;
     Color c;
 
-    public Hit(Double t, Point x, Vector n, Color c){
+    public Hit(Double t, Point x, Direction n, Color c){
         this.t = t;
         this.x = x;
         this.n = n;
