c7621411bf1d2bb74bd7c523bdb53aeac9c5255e
Lukas Sekoulidis <84957@beuth-hochschule.de>
2021-05-08 16:24:18 +0200

    Raytracer für a04 angepasst

---
+++ src/cgg/a04/Raytracing.java
@@ -0,0 +1,31 @@
+package cgg.a04;
+
+import cgg.*;
+import cgtools.*;
+import static cgtools.Vector.*;
+import static cgtools.Color.*;
+
+public class Raytracing implements Sampler{
+    Group group;
+    Lochkamera camera;
+
+    public Raytracing(Group group, Lochkamera camera){
+        this.group = group;
+        this.camera = camera;
+    }
+
+    public Color getColor(double x, double y) {
+        Ray ray = camera.Strahl(x, y);
+        Hit hit = group.intersect(ray);
+        return lightSurface(hit.getN(), hit.getC());
+    }
+
+    static Color lightSurface(Direction normal, Color color){
+        Direction lightDir = normalize(direction(1, 1, 0.5));
+        Color ambient = multiply(0.1, color);
+        Color diffuse = multiply(0.9 * Math.max(0, dotProduct(lightDir, normal)), color);
+        return add(ambient, diffuse);
+    }
+}
+
+
