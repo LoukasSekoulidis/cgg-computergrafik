bdffa723e69a94adddab748e59a5604d65e7a746
Lukas Sekoulidis <84957@beuth-hochschule.de>
2021-05-14 18:25:59 +0200

    Klassen angepasst für neue Aufgabe

---
+++ src/cgg/a05/Group.java
@@ -0,0 +1,26 @@
+package cgg.a05;
+
+import java.util.ArrayList;
+
+public class Group implements Shape{
+    ArrayList<Shape> shapes;
+
+    public Group(ArrayList<Shape> shapes){   
+        this.shapes = shapes;
+    }
+    
+    public Hit intersect(Ray r) {
+        double comp = Double.POSITIVE_INFINITY;
+        Hit hitRes = null;
+        for(Shape s : shapes){
+            Hit hitComp = s.intersect(r);
+            if(hitComp != null){
+                if(hitComp.t <= comp){
+                    comp = hitComp.t;
+                    hitRes = hitComp;
+                }
+            }
+        }
+        return hitRes;
+    }
+}
